ee515ead0c1f0f2803ffcbde835c9956be6601f1
William Friend <79694467+friendywill@users.noreply.github.com>
2021-03-05 13:36:19 +1100

    Update light_reactor.py

--- Laboratory 1/light_reactor.py
+++ Laboratory 1/light_reactor.py
@@ -8,66 +8,25 @@ from sphero_sdk import SerialAsyncDal
 from sphero_sdk import RvrStreamingServices
 
 
-loop = asyncio.get_event_loop()
+def main():
+	# This program demonstrates how to enable a single sensor to stream.
+	await rvr.wake()
+	# Give RVR time to wake up
+	await asyncio.sleep(2)
 
-rvr = SpheroRvrAsync(
-    dal=SerialAsyncDal(
-        loop
-    )
-)
+	await rvr.sensor_control.add_sensor_data_handler(
+		service=RvrStreamingServices.ambient_light,
+		handler=ambient_light_handler
+	)
 
-
-async def ambient_light_handler(ambient_light_data):
-
-
-async def main():
-	def ambient():
-		# This program demonstrates how to enable a single sensor to stream.
-
-		await rvr.wake()
-
-		# Give RVR time to wake up
-		await asyncio.sleep(2)
-
-		await rvr.sensor_control.add_sensor_data_handler(
-			service=RvrStreamingServices.accelerometer,
-			handler=ambient_light_handler
-		)
-
-		await rvr.sensor_control.start(interval=250)
-
-		# The asyncio loop will run forever to allow infinite streaming.
+	await rvr.sensor_control.start(interval=250)
+	await asyncio.sleep(1)
+	await rvr.set_all_leds(
+		led_group=RvrLedGroups.all_lights.value,
+		led_brightness_values=[color for x in range(10) for color in [255, 255, 255]]
+	)
 	
-		# This program demonstrates how to set the all the LEDs.
-	async def led():
-
-		await rvr.wake()
-
-		# Give RVR time to wake up
-		await asyncio.sleep(2)
-
-		await rvr.set_all_leds(
-			led_group=RvrLedGroups.all_lights.value,
-			led_brightness_values=[color for _ in range(10) for color in Colors.off.value]
-		)
-
-		# Delay to show LEDs change
-		await asyncio.sleep(1)
-
-		await rvr.set_all_leds(
-			led_group=RvrLedGroups.all_lights.value,
-			led_brightness_values=[color for x in range(10) for color in [255, 255, 255]]
-		)
-
-		# Delay to show LEDs change
-		await asyncio.sleep(1)
-
-		await rvr.close()
 	
-	def reactor():
-		
-
-
 if __name__ == '__main__':
     try:
         asyncio.ensure_future(
